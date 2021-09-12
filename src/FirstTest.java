671348d6afaa433a5587f2c9c648a889541c5495
SmallSparkle <a.kortikova@gmail.com>
2021-09-12 09:48:41 +0200

    added firstTest & wait method

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -15,24 +19,38 @@ public class FirstTest {
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("daviceName", "Wiki");
-        capabilities.setCapability("platformVersion", "10.0");
+        capabilities.setCapability("deviceName", "Wiki");
+        capabilities.setCapability("platformVersion", "10");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", "main.MainActivity");
         capabilities.setCapability("app",
-                "/Users/anastasiiabespalova/Desktop/JAvaAppiumAutomation/apks/org.wikipedia.apk");
+                "/Users/anastasiiabespalova/IdeaProjects/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
     public void firstTest() {
-        System.out.println("Fierst test run");
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_to_init_search.click();
+        WebElement element_to_enter_search_line = waitForElementByXpath(
+                "//*[contains(@text, 'Search…')]",
+                "Cannot Fins search input",
+                5
+        );
+        element_to_enter_search_line.sendKeys("Appium");
+    }
+
+    private WebElement waitForElementByXpath(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
